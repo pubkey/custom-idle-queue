59368434aa6c5753caa9d0c7441ef06db7602cdc
pubkey <daniel_van_meyer@hotmail.de>
2018-01-26 20:00:40 +0100

    ADD better profiling

--- test/performance.test.js
+++ test/performance.test.js
@@ -2,26 +2,35 @@
  * this runs some performance-test and measures the time
  */
 
-const AsyncTestUtil = require('async-test-util');
 const assert = require('assert');
 const IdleQueue = require('../');
 
 
 console.log('# start performance-test:');
 
-
+let count = 0;
+const getWaitPromise = () => {
+    return new Promise(res => {
+        setTimeout(() => {
+            res();
+            count++;
+        }, 1);
+    });
+};
+let count3 = 0;
 
 const run = async () => {
     console.time('aa');
     const queue = new IdleQueue();
 
-    let count = 0;
+    for (let i = 0; i < 10000; i++) {
+        queue.wrapCall(
+            () => count3++
+        );
+    }
 
     const wrappedCall = () => queue.wrapCall(
-        async () => {
-            await AsyncTestUtil.wait(10);
-            count++;
-        }
+        getWaitPromise
     );
 
     new Array(100000).fill(0).forEach(() => wrappedCall());
@@ -36,9 +45,9 @@ const run = async () => {
     await Promise.all(
         new Array(1000)
         .fill(0)
-        .map(async () => {
-            await queue.requestIdlePromise();
-            count2++;
+        .map(() => {
+            return queue.requestIdlePromise()
+                .then(() => count2++);
         })
     );
     assert.equal(count2, 1000);
