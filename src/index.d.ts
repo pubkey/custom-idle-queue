a051086e7633ee86b75ec95f1b28443c10c1be4b
Alf Eaton <hubgit@users.noreply.github.com>
2018-03-13 13:23:42 +0000

    Add missing return-type annotations

--- src/index.d.ts
+++ src/index.d.ts
@@ -12,10 +12,10 @@ declare class IdleQueue {
     wrapCall(fun: Function): Promise<any>;
 
     requestIdlePromise(options?: Options): Promise<void>;
-    cancelIdlePromise(prom: Promise<void>);
+    cancelIdlePromise(prom: Promise<void>): void;
 
     requestIdleCallback(cb: Function, options: Options): number;
-    cancelIdleCallback(handle: number);
+    cancelIdleCallback(handle: number): void;
 
     clear(): void;
 }
