11f3c24546e2f5d7f3d2d8708771bff4835ceeee
pubkey <daniel_van_meyer@hotmail.de>
2018-07-16 20:33:43 +0200

    ADD better performance-test

--- test/performance.test.js
+++ test/performance.test.js
@@ -2,61 +2,109 @@
  * this runs some performance-test and measures the time
  */
 
+
+const AsyncTestUtil = require('async-test-util');
 const assert = require('assert');
 const IdleQueue = require('../');
 
+const benchmark = {
+    wrapCalls: {},
+    deepIdle: {}
+};
 
-console.log('# start performance-test:');
+const elapsedTime = before => {
+    return AsyncTestUtil.performanceNow() - before;
+};
 
-let count = 0;
-const getWaitPromise = () => {
+const getWaitPromise = (time = 1) => {
     return new Promise(res => {
         setTimeout(() => {
             res();
-            count++;
-        }, 1);
+        }, time);
     });
 };
-let count3 = 0;
 
-const run = async () => {
-    console.time('aa');
-    const queue = new IdleQueue();
+describe('performance.test.js', () => {
 
-    for (let i = 0; i < 10000; i++) {
-        queue.wrapCall(
-            () => count3++
+    /**
+     * add many wrapCalls and await many requestIdlePromises
+     */
+    it('wrapCalls', async () => {
+        let count = 0;
+        let count3 = 0;
+
+        const startTime = AsyncTestUtil.performanceNow();
+        const queue = new IdleQueue();
+
+        for (let i = 0; i < 10000; i++) {
+            queue.wrapCall(
+                () => count3++
+            );
+        }
+
+        const wrappedCall = () => queue.wrapCall(
+            () => getWaitPromise().then(() => count++)
         );
-    }
-
-    const wrappedCall = () => queue.wrapCall(
-        getWaitPromise
-    );
 
 
-    await queue.requestIdlePromise();
-    for (let i = 0; i < 100000; i++)
-        wrappedCall();
+        await queue.requestIdlePromise();
+        for (let i = 0; i < 100000; i++)
+            wrappedCall();
 
-    console.log('# run wrapped calls');
-    await queue.requestIdlePromise();
-    assert.equal(count, 100000);
+        console.log('# run wrapped calls');
+        await queue.requestIdlePromise();
+        assert.equal(count, 100000);
 
-    await queue.requestIdlePromise();
+        await queue.requestIdlePromise();
 
-    console.log('# await idle-promises');
-    let count2 = 0;
-    await Promise.all(
-        new Array(1000)
-        .fill(0)
-        .map(() => {
-            return queue.requestIdlePromise()
-                .then(() => count2++);
-        })
-    );
-    assert.equal(count2, 1000);
+        console.log('# await idle-promises');
+        let count2 = 0;
+        await Promise.all(
+            new Array(1000)
+                .fill(0)
+                .map(() => {
+                    return queue.requestIdlePromise()
+                        .then(() => count2++);
+                })
+        );
+        assert.equal(count2, 1000);
 
-    console.timeEnd('aa');
-};
+        const elapsed = elapsedTime(startTime);
+        benchmark.wrapCalls = elapsed;
+    });
 
-run();
+    /**
+    * add one long wrapCall
+    * and await many idlePromises
+    */
+    it('deepIdle', async () => {
+        const queue = new IdleQueue();
+        let startTime = null;
+        queue.wrapCall(
+            () => getWaitPromise(300).then(() => startTime = AsyncTestUtil.performanceNow())
+        );
+
+
+        return new Promise(res => {
+            let amount = 1000;
+            for (let i = 0; i < amount; i++) {
+                queue.requestIdlePromise().then(() => {
+                    amount--;
+                    if (amount === 0) done();
+                });
+            }
+            function done() {
+                const elapsed = elapsedTime(startTime);
+                benchmark.deepIdle = elapsed;
+                res();
+            }
+        });
+
+
+    });
+
+    it('show result', () => {
+        console.log('benchmark result:');
+        console.log(JSON.stringify(benchmark, null, 2));
+    });
+});
